d11057ae68551413e5668082cfad7d0c1c31ef2b
D.Gelov <95240099+mrwinner04@users.noreply.github.com>
2022-05-06 13:38:25 +0300

    Update EstateConverter.java

--- src/main/java/com/bigProject/RealEstate/Converter/EstateConverter.java
+++ src/main/java/com/bigProject/RealEstate/Converter/EstateConverter.java
@@ -10,8 +10,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class EstateConverter {
     public Estate toEstate( EstateDto estateDto) {
-        return Estate.builder().city(City.builder().id(estateDto.getId()).build()).floor(Floor.builder().id(estateDto.getId()).build())
-                .description(estateDto.getDescription()).quadrature(Quadrature.builder().pureArea(estateDto.getPureArea()).builtUpArea(estateDto.getBuiltUpArea()).build())
+        return Estate.builder()
+            .city(City.builder()
+                  .id(estateDto.getId())
+                  .build())
+            .floor(Floor.builder()
+                   .id(estateDto.getId())
+                   .build())
+                .description(estateDto.getDescription())
+            .quadrature(Quadrature.builder()
+                        .pureArea(estateDto.getPureArea())
+                        .builtUpArea(estateDto.getBuiltUpArea())
+                        .build())
 
                 .estateFeatures(estateDto.getEstateFeatureIds().stream().map()).build();
     }
